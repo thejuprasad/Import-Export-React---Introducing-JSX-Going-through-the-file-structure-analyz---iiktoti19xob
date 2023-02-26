fede1c5b512b5fb8ee7590471ca1d38368551ca3
thejuprasad <73605401+thejuprasad@users.noreply.github.com>
2023-02-27 04:04:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,10 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <Heading />
+      <subHeading />
+      <InputQuery />
+      <submitButton />
     </div>
   )
 }
