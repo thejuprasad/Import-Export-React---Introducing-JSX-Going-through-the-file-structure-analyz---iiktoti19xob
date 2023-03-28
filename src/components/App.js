0e4e769f125a1047ed3cda0cc2984726b2c9548b
thejuprasad <73605401+thejuprasad@users.noreply.github.com>
2023-03-28 17:03:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,17 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
-import Heading from './Heading';
-import InputQuery from './InputQuery';
-import subHeading from './subHeading';
-import SubmitButton from './SubmitButton';
-
+import Heading from "./Heading";
+import InputQuery from "./InputQuery";
+import SubHeading from "./SubHeading";
+import SubmitButton from "./SubmitButton";
 const App = () => {
   return (
     <div id="main">
       <Heading />
-      <subHeading />
+      <SubHeading />
       <InputQuery />
       <SubmitButton />
     </div>
   )
 }
-
-
+export default App;
